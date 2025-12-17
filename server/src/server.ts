d69a2187c5f027b089b61e5e620014688b4b792e
MentaalAchtergesteld <mentaalachtergesteld@mentaalachtergesteld.nl>
2025-12-17 02:49:28 +0100

    layed out protocol

--- server/src/server.ts
+++ server/src/server.ts
@@ -19,6 +19,30 @@ const PORT = 9000;
 let rooms: Room[] = [];
 
 io.on("connection", (socket: Socket) => {
+	// Supposed connection flow
+	// (C2S) JoinQueue -> Matchmaking -> (S2C) JoinMatch -> (S2C) Seed -> (C2S) Ready ->
+	// Loop until max points are given out and winner is decided:
+	// [ (S2C) StartMatch -> Loop until game end: [ (C2S) Action -> (S2C) State/Action/Garbage ] -> (S2C) EndMatch ] ->
+	// (S2C) FinishMatch -> Wait until all players left: [ (C2S) -> LeaveMatch ] -> CloseRoom 
+	//
+	// Required packets:
+	// (C2S) JoinQueue (possible user ID for when accounts are added)
+	// (S2C) JoinMatch (with opponent ID, possible opponent data although that could be retrieved through another endpoint, and other match data like points to score)
+	// (S2C) Seed (for matching deterministic randomness)
+	// (C2S) Ready (done using socket, not provided user ID)
+	// (S2C) StartMatch
+	// (C2S) Action (GameAction)
+	// (S2C) SelfState (state of the player itself, for syncing)
+	// (S2C) OppState (state of the opponent, for syncing)
+	// (S2C) GarbageIn (when receiving garbage from opponent)
+	// (S2C) GarbageOut (when sending out garbage)
+	// (S2C) EndMatch (with winner ID)
+	// (S2C) FinishMatch (sent when all games have been played, with points scored and final winner)
+	// (C2S) LeaveMatch
+	// POSSIBLE FUTURE ADDITIONS:
+	// (C2S) ChatMessage (for in-game chat)
+	// (S2C) ChatMessage (for in-game chat)
+	
 	const player = new Player(socket);
 	console.log(`New connection: ${player.id} (${player.name})`);
 
