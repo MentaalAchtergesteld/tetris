03162fb060a84c0d64643903ca78b8643b7b3968
MentaalAchtergesteld <mentaalachtergesteld@mentaalachtergesteld.nl>
2025-12-18 00:12:47 +0100

    start working on packet handling on the client

--- client/src/game/modes/multiplayer.ts
+++ client/src/game/modes/multiplayer.ts
@@ -1,16 +1,17 @@
 import { io, Socket } from "socket.io-client";
 import { GameTheme } from "../../theme";
 import { GameContext, GameMode } from "../modes";
-import { C2SEvents, Game, S2CEvents } from "@tetris/shared";
+import { C2SEvents, Game, GameAction, PacketType, S2CEvents } from "@tetris/shared";
 import { LocalController } from "../../engine/input/controller";
 import { InputManager } from "../../engine/input/input_manager";
 import { DangerLevel } from "../danger";
 import { Widget } from "../../ui/widget";
 import { Recoil, Shaker } from "../../ui/widgets/effects";
-import { HBox, Overlay } from "../../ui/widgets/layout";
-import { Switch } from "../../ui/widgets/logic";
+import { Center, HBox, Overlay, SizedBox, VBox } from "../../ui/widgets/layout";
+import { Conditional, Switch } from "../../ui/widgets/logic";
 import { StandardGame } from "../../ui/widgets/standard_game";
 import { Label } from "../../ui/widgets/label";
+import { ColorBlock } from "../../ui/widgets/color_block";
 
 enum GameState {
 	JoiningQueue,
@@ -30,6 +31,7 @@ export class MultiplayerMode implements GameMode {
 	private input: InputManager;
 	private context: GameContext | null = null;
 	private countdownTimer: number = 0;
+	private hasWonlastMatch: boolean = false;
 
 	private layout: Widget;
 	private dangerLevel: DangerLevel;
@@ -57,9 +59,74 @@ export class MultiplayerMode implements GameMode {
 		this.joinQueue();
 	}
 
-	private joinQueue() {}
+	private joinQueue() {
+		this.socket.connect();
+		this.socket.emit(PacketType.JoinQueue);
+		this.state = GameState.InQueue;
+	}
 
-	private bindEvents() {}
+	private bindEvents() {
+		this.socket.on(PacketType.JoinRoom, () => {
+			this.state = GameState.InRoom;
+		});
+
+		this.socket.on(PacketType.PlayerJoined, (packet) => {
+			console.log(`Player ${packet.playerId} joined the room.`);
+		});
+
+		this.socket.on(PacketType.Seed, (packet) => {
+			this.localGame.setSeed(packet.seed);
+			this.remoteGame.setSeed(packet.seed);
+
+			this.socket.emit(PacketType.Ready);
+		});
+
+		this.socket.on(PacketType.StartMatch, () => {
+			this.state = GameState.InMatch;
+		});
+
+		this.socket.on(PacketType.Action, (packet) => {
+			if (this.state != GameState.InMatch) return;
+			if (packet.action == GameAction.SoftDrop) {
+				this.remoteGame.softDropFactor = packet.data;
+			} else {
+				this.remoteGame.softDropFactor = 1;
+				this.remoteGame.handleInput(packet.action);
+			}
+		});
+
+		this.socket.on(PacketType.SelfState,    (packet) => {
+			
+		});
+
+		this.socket.on(PacketType.OppState,     (packet) => {
+			this.remoteGame.setGrid(packet.grid);
+
+			const gameCurrentPiece = this.remoteGame.getCurrentPiece();
+			if (!gameCurrentPiece || !packet.currentPiece) return;
+
+			const xDistance = Math.abs(gameCurrentPiece.x - packet.currentPiece.x);
+			const yDistance = Math.abs(gameCurrentPiece.y - packet.currentPiece.y);
+
+			if (packet.currentPiece.rotation != gameCurrentPiece.rotation || xDistance > 5 || yDistance > 5) {
+				this.remoteGame.currentPiece = packet.currentPiece;
+			}
+		});
+
+		this.socket.on(PacketType.GarbageIn,    (packet) => {
+			this.localGame.addGarbage(packet.amount);
+		});
+
+		this.socket.on(PacketType.GarbageOut,   (packet) => {});
+
+		this.socket.on(PacketType.EndMatch,     (packet) => {
+			this.state = GameState.AfterMatch;
+		});
+
+		this.socket.on(PacketType.FinishMatch,  (packet) => {
+			this.state = GameState.FinishMatch;
+		});
+	}
 
 	private buildGameLayout(game: Game, isLocal: boolean): Widget {
 		const labelText = isLocal ? "you" : "other";
@@ -69,16 +136,20 @@ export class MultiplayerMode implements GameMode {
 		], dangerProvider, () => this.countdownTimer+1);
 	}
 
-	private buildRoomLayout(): Widget {}
-
-	private buildResultsLayout(): Widget {}
+	private buildResultsLayout(): Widget {
+		return new Overlay([
+			new ColorBlock("rgba(0, 0, 0, .75)"),
+			new Switch(() => this.hasWonlastMatch ? 1 : 0, [
+				new Label(() => "match won", "title", "center").setExpand(true),
+				new Label(() => "match lost", "title", "center").setExpand(true),
+			])
+		]);
+	}
 
 	private buildLayout(): Widget {
 		const localGame = this.buildGameLayout(this.localGame, true);
 		const remoteGame = this.buildGameLayout(this.remoteGame, false);
 		const gameLayout = new HBox([localGame, remoteGame], 64);
-
-		const roomLayout = this.buildRoomLayout();
 
 		const lobbyOrGameLayout = new Switch(() => {
 			switch (this.state) {
@@ -90,7 +161,7 @@ export class MultiplayerMode implements GameMode {
 		},[
 			new Label(() => "joining queue", "title", "center"),
 			new Label(() => "in queue", "title", "center"),
-			roomLayout,
+			new Label(() => "in room", "title", "center"),
 			gameLayout
 		]);
 
